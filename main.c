c090b8ba13158757abe598da208fc7acf0b66dd9
metn-u <metincan.avar21@gmail.com>
2025-12-25 17:56:58 +0300

    .gitignore eklendi ve main.c güncellendi

--- main.c
+++ main.c
@@ -3,10 +3,35 @@
 #include <string.h>
 
 
+void menu(){
+    printf("--- OTÖBÜS BİLET SİSTEMİ ---\n");
+    printf("1. Yeni Sefer Oluştur\n"); // [cite: 13]
+    printf("2. Seferleri Listele\n");   // [cite: 17]
+    printf("3. Bilet Satış\n");         // [cite: 18]
+    printf("0. Çıkış\n");
+    printf("Seçiminiz: ");
+
+
+}
+
+typedef struct{
+    int ID;
+    char[] Departure;
+    char[] Arrival;
+    char[] Date;
+    char[] DepartureTime;
+    char[] BusPlate;
+    char[] DriverName;
+    int Seats;
+}Trip;
+
+
 
 int main() {
 
     printf("Welcome To Bus Ticketing System\n");
-    
+
+    menu();
+
     return 0;
 }
